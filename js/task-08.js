52889f121c94ce37142879db3df62ae6d7812fc1
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-02 15:11:39 +0300

    upd

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', (event) => {  
+    event.preventDefault();
+    let { email, password } = event.target.elements;
+    email = email.value;
+    password = password.value;
+    if (!email || !password) {
+        alert('Виявлено незаповнене поле!');
+    } else {
+        const resultObj = { email, password };
+        console.log(resultObj);
+        form.reset();
+    };
+});
