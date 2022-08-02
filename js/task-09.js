20f63060416a85058d0a55fdc1f8ed2fcbd5ea91
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-02 15:24:53 +0300

    upd

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+const btn = document.querySelector('.change-color');
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  document.querySelector('body').style.backgroundColor = `${color}`;
+  document.querySelector('.color').innerHTML = `${color}`;
+});
