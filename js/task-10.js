3e1a59f7c42f82c65375fdd0a66e5ce46d414014
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-02 17:44:50 +0300

    upd

--- js/task-10.js
+++ js/task-10.js
@@ -2,13 +2,13 @@ const amount = document.querySelector('input');
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
 let boxesAcc = '';
+let size = 30;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
 function createBoxes(amount) {
-  let size = 30;
   for (let i = 0; i < amount.value; i++) {
     boxesAcc += `<div style="background-color:${getRandomHexColor()}; width:${size}px; height:${size}px"></div>`;
     size += 10;
@@ -20,6 +20,7 @@ function createBoxes(amount) {
 function destroyBoxes() {
   document.querySelector('#boxes').innerHTML = '';
   boxesAcc = '';
+  size = 30;
 };
 
 createBtn.addEventListener('click', () => createBoxes(amount));
