57c56ed5865aba804089293ec9bef1265c059c0e
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-02 17:40:01 +0300

    upd

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('#validation-input');
 input.addEventListener('blur', (event) => {
     Number(input.getAttribute('data-length')) === event.target.value.length
-        ? input.setAttribute('id', 'validation-input.valid')
-        : input.setAttribute('id', 'validation-input.invalid')
+        ? input.setAttribute('class', 'valid')
+        : input.setAttribute('class', 'invalid')
 });
